a5a3f5a6b1507d22e3a6e61211223768c214e38d
Matkija <86783808+matkijahenkilo@users.noreply.github.com>
2023-11-17 07:38:13 -0300

    Add paranoia of making my hard drive worse than it already is

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -3,3 +3,4 @@
 grim -t png ~/.config/hypr/scripts/lock.png
 magick ~/.config/hypr/scripts/lock.png -blur 10,10 ~/.config/hypr/scripts/lock.png
 swaylock -f -c 110000 -i ~/.config/hypr/scripts/lock.png
+rm ~/.config/hypr/scripts/lock.png
